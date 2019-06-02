8a802142cd27f49b47efe7c3a650a28e2fe10a91
Michael Garrett <garrettux@gmail.com>
2019-06-02 13:44:57 -0400

    first pass at building an app image
    yarn start:server
    
    use service discovery name for rethinkdb
    
    mess with travis
    
    use https
    
    mess with testing in travis
    
    logs
    
    curl
    
    do a dang ol curl
    
    CI/CD
    
    deploy doesn't mean what i thought it did

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+RETHINKDB_HOST=${RETHINKDB_HOST:-localhost}
+
 # echo "Waiting for RethinkDB to become available"
-while ! nc -z localhost 28015; do sleep 3; done
+while ! nc -z $RETHINKDB_HOST 28015; do sleep 3; done
 
 # Let's sleep 10 seconds to wait for the DB to be ready
 # sleep 10
