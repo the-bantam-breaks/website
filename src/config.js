3038bb9d5d09d2dc7cd9cafe6e2083f8f8157237
Michael Garrett <garrettux@gmail.com>
2019-06-03 17:54:19 -0400

    hardcode rethinkdb addr
    for some reason it isn't respecting the env var

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const { env } = process;
 export const APP_PORT = env.PORT || 3000;
 
 export const RETHINKDB = {
-    HOST: env.RETHINKDB_HOST || 'rethinkdb',
+    HOST: env.RETHINKDB_HOST || 'rethinkdb.local',
     PORT: env.RETHINKDB_PORT || 28015,
     DATABASE: 'breaksWebsite'
 };
