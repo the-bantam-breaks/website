f4701168a7d3f5d3b782bc1361d29306d8f203b5
Michael Garrett <michael.garrett@greensky.com>
2019-06-04 13:26:25 -0400

    fix build
    * remove node frmo travis build env
    * allow rethinkdb config passed to webpack as env vars
    
    this can be localhost again since env vars work

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const { env } = process;
 export const APP_PORT = env.PORT || 3000;
 
 export const RETHINKDB = {
-    HOST: env.RETHINKDB_HOST || 'rethinkdb.local',
+    HOST: env.RETHINKDB_HOST || 'localhost',
     PORT: env.RETHINKDB_PORT || 28015,
     DATABASE: 'breaksWebsite'
 };
