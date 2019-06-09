871566fcc5d047794c698fa9d20bb2cd0b9bbb92
Scotland Stephenson <scotlandrummer@yahoo.com>
2019-06-09 09:50:37 -0400

    convert insta url to using env vars

--- src/server/controllers/instagram.js
+++ src/server/controllers/instagram.js
@@ -2,7 +2,7 @@ import got from 'got';
 
 // TODO - Garrett set TOKEN in env vars
 const ENV_INSTA_TOKEN = '';
-const INSTAGRAM_API_URL = `https://api.instagram.com/v1/users/3018065801/media/recent/?access_token=${ENV_INSTA_TOKEN}`;
+const INSTAGRAM_API_URL = `https://api.instagram.com/v1/users/${ENV_INSTAGRAM_USER_ID}/media/recent/?access_token=${ENV_INSTAGRAM_TOKEN}`;
 
 export const instagram = {
     feed: async () => {
