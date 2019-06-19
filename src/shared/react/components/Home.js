d8eb5c9f4fb41d4d8a7acb71bcb8f01a7888e237
Scotland Stephenson <scotlandrummer@yahoo.com>
2019-06-19 01:26:21 -0400

    Does Stuff!!!
      - add app bootstrap data logic
      - add dynamic images
      - build hero, sticky(ish) nav, and Logo components

--- src/shared/react/components/Home.js
+++ src/shared/react/components/Home.js
@@ -1,13 +1,42 @@
-import React, { Fragment } from 'react';
-import { default as NavBar } from './NavBar';
+import React, { Component } from 'react';
+import { StickyContainer } from 'react-sticky';
+import { AppDataProvider } from '../context/';
+import { NavBar } from './NavBar';
+import { Hero } from './Hero';
 
-const Home = () => {
-    return (
-      <div>
-          <h3>Home Page</h3>
-          <NavBar />
-      </div>
-    );
-};
+class Home extends Component {
+    constructor (props) {
+        super(props)
+
+        let appData;
+
+        // eslint-disable-next-line
+        if (__isBrowser__) {
+            appData = window.__INITIAL_DATA__;
+            delete window.__INITIAL_DATA__;
+        } else {
+            appData = {}
+        }
+
+        this.state = {
+            appData
+        };
+    }
+
+    render () {
+        const { appData } = this.state
+        if (!appData) {
+            return null;
+        }
+        return (
+            <AppDataProvider value={appData}>
+                <StickyContainer>
+                    <Hero />
+                    <NavBar />
+                </StickyContainer>
+            </AppDataProvider>
+        );
+    }
+}
 
 export default Home;
