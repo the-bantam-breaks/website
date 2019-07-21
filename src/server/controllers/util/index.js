0c2f9f60d24dd7eb1ea967dca6f6db69855d7f37
Scotland Stephenson <scotlandrummer@yahoo.com>
2019-07-21 00:58:26 -0400

    move validators

--- src/server/controllers/util/index.js
+++ src/server/controllers/util/index.js
@@ -3,3 +3,5 @@ export * from './generate-token';
 export { getRandom } from './get-random';
 export * from './gmail-mail';
 export * from './slack-booking-webhook';
+export * from './validate-email';
+export * from './validate-phone';
