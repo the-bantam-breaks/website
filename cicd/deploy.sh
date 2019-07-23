1e3f03d20c1e612b01df44052e99335624228679
Scotland Stephenson <scotlandrummer@yahoo.com>
2019-07-23 11:05:13 -0400

    Develop to master.  update deps, add porchfest gig, fix shows, dev deploy (#77)
    * Update dependency koa-logger to v3.2.1
    
    * Add staging deploy (#64)
    
    * add second staging deploy from develop
    
    * more descriptive output
    
    * Update dependency webpack to v4.36.1
    
    * Update babel monorepo to v7.5.5
    
    * Update dependency aws-sdk to v2.496.0
    
    * Update dependency eslint-plugin-import to v2.18.2
    
    * Update dependency react-card-flip to v0.8.1
    
    * Update dependency eslint to v6.1.0
    
    * Update dependency aws-sdk to v2.497.0
    
    * Update dependency eslint-plugin-react to v7.14.3
    
    * add porchfest and fix upcoming show logic
    
    * add email logo img
    
    * Update dependency webpack to v4.37.0

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 TAG=$1
+SERVICE=$2
+AWSLOGS_GROUP="/ecs/${SERVICE}"
 REPOSITORY=796520576045.dkr.ecr.us-east-1.amazonaws.com/the-bantam-breaks/breaks-website
 IMAGE=${REPOSITORY}:${TAG}
 ECS_CLUSTER=bantam-breaks
-ECS_SERVICE=website
+ECS_SERVICE=$SERVICE
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TASK_JSON_TEMPLATE=${DIR}/task.json
 
 TASK_JSON=$(cat $TASK_JSON_TEMPLATE | \
-  jq --arg IMAGE "$IMAGE" '.taskDefinition | (.containerDefinitions[] | select(.name == "breaks-website") | .image) |= $IMAGE')
+  jq --arg IMAGE "$IMAGE" --arg LOGS "$AWSLOGS_GROUP" '.taskDefinition | (.containerDefinitions[] | select(.name == "breaks-website") | .image) |= $IMAGE | (.containerDefinitions[] | select(.name == "breaks-website") | .logConfiguration.options."awslogs-group") |= $LOGS')
 
 TASK_REVISION_ARN=$(aws ecs register-task-definition \
   --cli-input-json "$TASK_JSON" | \
